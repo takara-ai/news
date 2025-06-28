37818d01f2b2bc6c105c690ee7ee5c1798c8ef4e
Jacob Kenney <jacob.t.kenney1@gmail.com>
2025-06-28 14:53:56 +0100

    feat: editorial article writing agent

--- news.py
+++ news.py
@@ -4,20 +4,24 @@ from agent.tools.web import web_search_tool, parse_webpage_tool
 
 # Get a load of content for writing editorials
 def curate_content(query: str) -> str:
-    agent = CodeAgent("openai", "content_curator-2", loop_limit=10, tools=[web_search_tool, parse_webpage_tool], model="gpt-4.1")
-    return agent(question)
+    agent = CodeAgent("openai", "content_curator-3", loop_limit=10, tools=[web_search_tool, parse_webpage_tool], model="gpt-4.1-mini")
+    return agent(query)
 
-# TODO: prompt
-def create_article(research: str) -> str:
-    agent = CodeAgent("openai", "editorial_writer", loop_limit=10, tools=[web_search_tool, parse_webpage_tool], model="gpt-4.1")
+# Create an article from research
+def create_article(research: str, model: str = "gpt") -> str:
+    if model == "gpt":
+        agent = CodeAgent("openai", "editorial_writer-2", loop_limit=10, tools=[web_search_tool, parse_webpage_tool], model="gpt-4.1-mini")   
+    elif model == "gemini":
+        agent = CodeAgent("google", "editorial_writer-2", loop_limit=10, tools=[web_search_tool, parse_webpage_tool], model="gemini-2.5-pro")
     return agent(research)
 
-
 if __name__ == "__main__":
-    # Initialise agent:
-    content_creation_agent = CodeAgent("openai", "content_curator-2", loop_limit=10, tools=[web_search_tool, parse_webpage_tool], model="gpt-4.1")
     question = "Breaking, hype tech news about AI" # Test question
-    print(content_creation_agent.system_prompt)
-    answer = content_creation_agent(question, debug=True, eval_check=True)
-    print(f"\n\nANSWER: {answer}\n\n")
-    pause = input("\nContinue?:\n")
+    research = curate_content(question)
+    print(research)
+    agent = CodeAgent("google", "editorial_writer-2", loop_limit=10, tools=[web_search_tool, parse_webpage_tool], model="gemini-2.5-pro")
+    print(f"System Prompt: {code_agent.system_prompt}")
+    agent(research, debug=True, eval_check=True)
+    print(f"\n\nANSWER: {article}\n\n")
+    #article = create_article(question, "gemini")
+    print(article)

7074bc4abba9e9ee23e89463b4d48912987c07d6
Mikus Sturmanis <mikussturmanis1@gmail.com>
2025-06-28 15:49:47 +0100

    asdfghjkl

--- utils.py
+++ utils.py
@@ -19,7 +19,7 @@ def generate_news_schema(content:str):
     NEWS_SCHEMA_SYS="""Give an Unstructured News parsed for various sources your role is to structure the news article in proper format:" 
     Here is the unstrcutured content {content}"""
 
-    completion = client.chat.completions.parse(
+    completion = client.chat.completions.create(
         model="gpt-4.1",
         messages=[
             {"role": "system", "content": NEWS_SCHEMA_SYS},

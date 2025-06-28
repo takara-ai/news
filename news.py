971d8249faec5ee6f42761e4af73ce696416d3b8
Jacob Kenney <jacob.t.kenney1@gmail.com>
2025-06-28 14:02:51 +0100

    wip: editorial writing agent

--- news.py
+++ news.py
@@ -2,6 +2,17 @@ from agent.core.code_agent import CodeAgent
 from agent.core.openai import OpenAIAgent
 from agent.tools.web import web_search_tool, parse_webpage_tool
 
+# Get a load of content for writing editorials
+def curate_content(query: str) -> str:
+    agent = CodeAgent("openai", "content_curator-2", loop_limit=10, tools=[web_search_tool, parse_webpage_tool], model="gpt-4.1")
+    return agent(question)
+
+# TODO: prompt
+def create_article(research: str) -> str:
+    agent = CodeAgent("openai", "editorial_writer", loop_limit=10, tools=[web_search_tool, parse_webpage_tool], model="gpt-4.1")
+    return agent(research)
+
+
 if __name__ == "__main__":
     # Initialise agent:
     content_creation_agent = CodeAgent("openai", "content_curator-2", loop_limit=10, tools=[web_search_tool, parse_webpage_tool], model="gpt-4.1")
